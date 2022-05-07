ca652a1b088d36d43b31c3865d9776a1bc36c37c
asalah <90047716+asalahhh@users.noreply.github.com>
2022-05-07 20:36:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,49 @@
 document.getElementById("loginform").onsubmit = function () {
     var user = document.getElementById("user").value;
     var pass = document.getElementById("pass").value;
-    if ( ((user == localStorage.newUser) && (pass == localStorage.newPass)) || (((user == "salah@123") && (pass == "987654321"))) ) {
+    if (((user == localStorage.newUser) && (pass == localStorage.newPass)) || (((user == "salah@123") && (pass == "987654321")))) {
         return true;
     } else {
+        document.getElementById("wrong").textContent = "invalid inputs";
         return false;
     }
 }
+document.getElementById("loginform").onsubmit = function () {
+    var a = document.getElementById("user");
+    var b = document.getElementById("pass");
+    if (a.value === "") {
+        document.getElementById("wrong").textContent = "please enter username";
+        return false;
+    }
+    else if (b.value === "" && b.value.length <= 10) {
+        document.getElementById("wrong").textContent = "please enter your password"
+        return false;
+
+    }
+
+
+};
+document.getElementById("eye").onclick = function () {
+    var x = document.getElementById("pass");
+    var y = document.getElementById("hide1");
+    var z = document.getElementById("hide2");
+    if (x.value === "") {
+
+        z.style.display = "none";
+    }
+    if (x.type === 'password') {
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else {
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+};
+let click = document.querySelector('.click');
+let list = document.querySelector('.list');
+click.addEventListener("click", () => {
+    list.classList.toggle('newlist');
+})
--- js/main2.js
+++ js/main2.js
@@ -1,8 +1,21 @@
 let username = document.getElementById("us1");
 let pass = document.getElementById("ps1");
-    username.onkeyup = function() {
-        localStorage.setItem('newUser',username.value);
+username.onkeyup = function () {
+    localStorage.setItem('newUser', username.value);
+};
+pass.onkeyup = function () {
+    localStorage.setItem('newPass', pass.value);
+};
+document.getElementById("reg").onsubmit = function () {
+    let conpas = document.getElementById("ps2");
+    let inputs = document.getElementsByClassName("inp");
+    if (inputs.value==="") {
+        document.getElementById("no").textContent = "please enter complete inputs";
+        return false;
     }
-    pass.onkeyup = function(){
-        localStorage.setItem('newPass',pass.value);
+    else if (pass.value !== conpas) {
+        document.getElementById("no").textContent = "please confirm your password";
+        return false;
+    }
+    
     }
