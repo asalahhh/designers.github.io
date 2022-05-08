552e0d7af436c31607c6f5bb728381c8a48cfed3
asalah <90047716+asalahhh@users.noreply.github.com>
2022-05-08 10:14:57 +0200

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -1,21 +1,8 @@
 let username = document.getElementById("us1");
 let pass = document.getElementById("ps1");
-username.onkeyup = function () {
-    localStorage.setItem('newUser', username.value);
-};
-pass.onkeyup = function () {
-    localStorage.setItem('newPass', pass.value);
-};
-document.getElementById("reg").onsubmit = function () {
-    let conpas = document.getElementById("ps2");
-    let inputs = document.getElementsByClassName("inp");
-    if (inputs.value==="") {
-        document.getElementById("no").textContent = "please enter complete inputs";
-        return false;
+    username.onkeyup = function() {
+        localStorage.setItem('newUser',username.value);
     }
-    else if (pass.value !== conpas) {
-        document.getElementById("no").textContent = "please confirm your password";
-        return false;
-    }
-    
+    pass.onkeyup = function(){
+        localStorage.setItem('newPass',pass.value);
     }
